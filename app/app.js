6863b40acbb939f26fd63789fe7b9b2409900e2e
Ken Simiyu <kensimiyu2015@gmail.com>
2018-02-12 15:36:08 +0300

    logic for appending version number infront of app title

--- app/app.js
+++ app/app.js
@@ -2,23 +2,40 @@
   // Create a variable called app and assign the module to it
   var app = angular.module('app', []);
 
+  app.provider('books', function () {
+    this.$get = function () {
+       
+     //string variables value's in the service
+       var appName = 'Data Science Online BookStore';
+       var appDesc = 'Track which books you have read.';
+
+       // application version
+       var version = '1.0';
+
+       // checking the includeVersionInTitle value if true it appends the version number
+       if (includeVersionInTitle) {
+         appName += '' + version;
+       }
+
+      // return an object literal representing the service
+       return {
+         appName: appName,
+         appDesc: appDesc
+       };
+      
+    };
+    // code for appending version in front of title
+    var includeVersionInTitle = false;
+    this.setIncludeVersionInTitle = function (value) {
+      includeVersionInTitle = value;
+    };       
+ });
+  
+  
   // use the variable to call the config function
-  app.config(function($provide){
+  app.config(function(booksProvider){
 
-    $provide.provider('books', function () {
-       this.$get = function () {
-          
-        //string variables value's in the service
-          var appName = 'Data Science Online BookStore';
-          var appDesc = 'Track which books you have read.';
+    booksProvider.setIncludeVersionInTitle(true);
 
-         // return an object literal representing the service
-          return {
-            appName: appName,
-            appDesc: appDesc
-          };
-         
-       };      
-    });
   });
 })();
